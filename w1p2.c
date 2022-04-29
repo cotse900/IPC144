44c041f00f615f94c5ee7371b5c1266af6140046
Chungon <78128902+cotse900@users.noreply.github.com>
2022-04-29 00:02:56 -0400

    Add files via upload

---
+++ w1p2.c
@@ -0,0 +1,11 @@
+/*Chung On TSE, 154928188, cotse@myseneca.ca, IPC144NFF*/
+#include<stdio.h>
+int main(void)
+{
+	printf("+-------------------------------+\n");
+	printf("| Introduction to C Programming |\n");
+	printf("+-------------------------------+\n");
+	printf("Program written by:\n");
+	printf("Chungon Tse\n");
+	return(0);
+}
